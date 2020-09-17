ca0a3f9a06255407d6c4232b143b249d0186379f
btate712 <btate712@gmail.com>
2020-09-16 19:30:52 -0500

    completed second task

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const changeUserToName = () => {
   const nameSlot = document.getElementById("name");
   nameSlot.innerText = name;
 }
+
+const align = direction => {
+  const h1 = document.querySelector("h1");
+  h1.className = direction;
+}
